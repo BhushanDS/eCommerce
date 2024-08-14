36dd7a0ee34bd787b184967a2508966cb5f62606
BhushanDS <bhushanrane1992@gmail.com>
2024-08-14 17:28:40 +0530

    Initial commit with local code

---
+++ Release1/backend/spring-boot-ecommerce/src/main/java/com/oneshop/ecommerce/dao/ProductRepository.java
@@ -0,0 +1,9 @@
+package com.oneshop.ecommerce.dao;
+
+import com.oneshop.ecommerce.entity.Product;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.web.bind.annotation.CrossOrigin;
+
+@CrossOrigin("http://localhost:4200")
+public interface ProductRepository extends JpaRepository<Product, Long> {
+}
